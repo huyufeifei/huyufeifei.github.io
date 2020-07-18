f23f6d7a78423f9a0ffe6cafaf5f30e28b653626
huyufeifei <3172281973@qq.com>
2020-07-18 20:19:10 +0800

    change detail

--- source/js/head.js
+++ source/js/head.js
@@ -1,4 +1,5 @@
 document.write("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">");
 document.write("  <link rel=\"stylesheet\" type=\"text\/css\" href=\"\/source\/css\/main.css\">");
+document.write("  <link rel=\"stylesheet\" type=\"text\/css\" href=\"\/source\/css\/github_markdown.css\">");
 document.write("  <link rel=\"shortcut icon\" href=\"\/source\/images\/favicon.ico\" \/>");
 document.write("  <link rel=\"bookmark\" href=\"\/source\/images\/favicon.ico\" \/>");
