19ac3ccf339b291540b8383ce2303f59ea6b7857
huyufeifei <3172281973@qq.com>
2020-07-20 17:52:05 +0800

    add articles page

--- source/js/head.js
+++ source/js/head.js
@@ -1,5 +1,6 @@
 document.write("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">");
-document.write("  <link rel=\"stylesheet\" type=\"text\/css\" href=\"\/source\/css\/main.css\">");
-document.write("  <link rel=\"stylesheet\" type=\"text\/css\" href=\"\/source\/css\/github_markdown.css\">");
-document.write("  <link rel=\"shortcut icon\" href=\"\/source\/images\/favicon.ico\" \/>");
-document.write("  <link rel=\"bookmark\" href=\"\/source\/images\/favicon.ico\" \/>");
+document.write("  <link rel=\"stylesheet\" type=\"text/css\" href=\"/source/css/main.css\">");
+document.write("  <link rel=\"stylesheet\" type=\"text/css\" href=\"/source/css/github_markdown.css\">");
+document.write("  <link rel=\"shortcut icon\" href=\"/source/images/favicon.ico\" />");
+document.write("  <link rel=\"bookmark\" href=\"/source/images/favicon.ico\" />");
+document.write("  <script src=\"https://cdn.jsdelivr.net/npm/marked/marked.min.js\"></script>")
