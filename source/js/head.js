50d15d1de8ccdb6c56adff9916f76fd594342bc3
huyufeifei <3172281973@qq.com>
2020-09-17 00:16:18 +0800

    add a image

--- source/js/head.js
+++ source/js/head.js
@@ -1,9 +1,11 @@
 function loadFile(filePath) {
+    console.log('in loadFile');
     var result = 'ERR';
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", filePath, false);
     xmlhttp.send();
-    if (xmlhttp.status==200) {
+    console.log(xmlhttp.status);
+    if (xmlhttp.status===200) {
         result = xmlhttp.responseText;
     }
     return result;
